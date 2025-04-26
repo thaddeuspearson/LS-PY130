6663636a207336baadc1b8b113e03de58e7a7767
Thaddeus <thaddeuspearson@gmail.com>
2025-04-27 00:40:22 +0200

    add unpack_a_list to Easy_2:

---
+++ Python_Foundations_More_Topics/Easy_2/unpack_a_list.py
@@ -0,0 +1,12 @@
+"""
+Given a list with four elements, unpack these elements into four separate
+variables.
+"""
+
+lst = [10, 20, 30, 40]
+first, second, third, fourth = lst
+
+assert first == 10
+assert second == 20
+assert third == 30
+assert fourth == 40
