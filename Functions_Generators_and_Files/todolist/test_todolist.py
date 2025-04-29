c7a5c2d987811d3947facf8eef8c3d68cf677d4b
Thaddeus <thaddeuspearson@gmail.com>
2025-04-29 21:39:03 +0200

    add test_length to todolist tests

---
+++ Functions_Generators_and_Files/todolist/test_todolist.py
@@ -0,0 +1,22 @@
+import unittest
+from todolist import Todo, TodoList
+
+
+class TestTodoList(unittest.TestCase):
+
+    def setUp(self):
+        self.todo1 = Todo("Buy milk")
+        self.todo2 = Todo("Clean room")
+        self.todo3 = Todo("Go to the gym")
+
+        self.todos = TodoList("Today's Todos")
+        self.todos.add(self.todo1)
+        self.todos.add(self.todo2)
+        self.todos.add(self.todo3)
+
+    def test_length(self):
+        self.assertEqual(3, len(self.todos))
+
+
+if __name__ == "__main__":
+    unittest.main()
