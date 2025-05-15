273cece4b59cc15b9a70edfbd6b09b01d8493ebf
Thaddeus <thaddeuspearson@gmail.com>
2025-05-15 14:54:16 +0200

    pylint fixes test_point_mutations.py

--- Python_Challenges/Easy_Challenges/test_point_mutations.py
+++ Python_Challenges/Easy_Challenges/test_point_mutations.py
@@ -37,5 +37,6 @@ class TestDNAMutations(unittest.TestCase):
         self.assertEqual(4, dna.hamming_distance("AGACATCTTTCAGCCGCCGGATTAGGCAA"))
         self.assertEqual(1, dna.hamming_distance("AGG"))
 
+
 if __name__ == "__main__":
     unittest.main()
